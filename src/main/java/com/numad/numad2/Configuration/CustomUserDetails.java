dcd8496ded52369431db8a69b0017dbe627db90c
Eager-ToLearn <oarabilebooo@gmail.com>
2024-05-15 12:31:06 +0200

    commit thousand

--- src/main/java/com/numad/numad2/Configuration/CustomUserDetails.java
+++ src/main/java/com/numad/numad2/Configuration/CustomUserDetails.java
@@ -1,52 +1,69 @@
 package com.numad.numad2.Configuration;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import com.numad.numad2.Model.User;
+
 public class CustomUserDetails implements UserDetails {
+	
+	
+	
+	private User user;
+	
+	
+
+	public CustomUserDetails(User user) {
+		this.user = user;
+	}
 
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+		
+		authorities.add(new SimpleGrantedAuthority(user.getRole().toString()));
+		
+		
+		return authorities;
 	}
 
 	@Override
 	public String getPassword() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return user.getPassword();
 	}
 
 	@Override
 	public String getUsername() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return user.getEmail();
 	}
 
 	@Override
 	public boolean isAccountNonExpired() {
-		// TODO Auto-generated method stub
-		return false;
+		
+		return true;
 	}
 
 	@Override
 	public boolean isAccountNonLocked() {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
 	public boolean isCredentialsNonExpired() {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
 	public boolean isEnabled() {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 }
